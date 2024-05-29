8dcbc2631a36c55c87b8a37d16efde68475ae775
bl0up <ambroise.m2@gmail.com>
2024-05-29 16:50:56 +0200

    feat: hybrid hardhat foundry

--- ignition/modules/ExampleERC721a.ts
+++ ignition/modules/ExampleERC721a.ts
@@ -5,7 +5,6 @@ const ExampleERC721aModule = buildModule('ExampleERC721aModule', (m) => {
     'ExampleNFT',
     'NFT',
     'ipfs://',
-    '0xa5409ec958C83C3f309868babACA7c86DCB077c1',
     '0x813af93e50F0bCD2BAaFfa7E4dD4710adC01dE7d',
   ]);
   return { erc721a };
